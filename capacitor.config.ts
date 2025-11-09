70baaafd9e38222c7dfea7208c82f6a5c1b80a87
MariusTNTX <molinamario.msc@gmail.com>
2025-11-09 17:47:18 +0100

    switch www into docs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'spanish-heavy-repository',
-  webDir: 'www'
+  webDir: 'docs'
 };
 
 export default config;
